39136bc5e93c706e1814dcf228bae2b96373aa9c
Ancor Gonzalez Sosa <ancor@suse.de>
2014-07-20 18:26:04 +0200

    rails new galleror

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Galleror::Application.config.secret_key_base = '7c055a99255c7ef017eee4f4b01449a4e801b6aa507604511bbad9a978fb30aa65083f2d89c84aefc85b924ad10f84de9b005a15f6cee2bc052ad47fe43d8d77'
