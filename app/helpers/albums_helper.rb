bf4f00814f7a9b3cd62da2ee8fb559eb7b3576ee
Imobach González Sosa <imobachgs@gmail.com>
2014-07-29 06:28:14 +0100

    Añade representación gráfica de la puntuación del álbum
    * Usa un icono de KDE 4 (face-star.png).

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,6 @@
 module AlbumsHelper
+  def format_rating(rating, image = 'face-star.png')
+    stars = image_tag(image, alt: '*') * rating
+    content_tag :span, stars.html_safe, class: 'rating'
+  end
 end
