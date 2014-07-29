26314349dcb6bbfc4406f9d955e7baa7de7abe38
Imobach González Sosa <imobachgs@gmail.com>
2014-07-29 06:37:30 +0100

    Refactoriza AlbumsController usando respond_with

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,6 @@
 class AlbumsController < ApplicationController
   before_action :set_album, only: [:show, :edit, :update, :destroy]
+  respond_to :html
 
   # GET /albums
   # GET /albums.json
@@ -24,41 +25,22 @@ class AlbumsController < ApplicationController
   # POST /albums
   # POST /albums.json
   def create
-    @album = Album.new(album_params)
-
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to albums_url, notice: 'Album was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @album }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
+    @album = Album.create(album_params)
+    respond_with(@album, location: albums_url)
   end
 
   # PATCH/PUT /albums/1
   # PATCH/PUT /albums/1.json
   def update
-    respond_to do |format|
-      if @album.update(album_params)
-        format.html { redirect_to albums_url, notice: 'Album was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
+    @album.update(album_params)
+    respond_with(@album, location: albums_url)
   end
 
   # DELETE /albums/1
   # DELETE /albums/1.json
   def destroy
     @album.destroy
-    respond_to do |format|
-      format.html { redirect_to albums_url }
-      format.json { head :no_content }
-    end
+    respond_with(@album)
   end
 
   private
