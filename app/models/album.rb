14937188e3ac124661e8db733a503b1038e8c95f
Imobach González Sosa <imobachgs@gmail.com>
2014-07-29 06:29:29 +0100

    Limita la vista de álbums a los públicos

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,6 @@ class Album < ActiveRecord::Base
   validates :title, presence: true
   validates :rating, numericality: { only_integer: true, allow_blank: true,
     greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  scope :public, -> { where(public: true) }
 end
